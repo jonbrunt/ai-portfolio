cb60c68d0ffee214688a810ceb1826b1a648e539
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-03-09 20:40:13 -0300

    HERO COMPONENT
    - in component/hero.tsx:
    - import Next's Image
    - create and export Hero functional component
    - use daisyUI `hero` component as base
    - add lorem ipsum in p element, button text, and h1 text
    - add headshot .jpg and properties to Image component
    - customize Tailwind for basic needs (update later)

---
+++ app/components/hero.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <Image
+          src="/brunt-headshot.jpg"
+          alt="Jonathan Brunt Headshot Picture"
+          width="384"
+          height="384"
+          className="max-w-xs rounded-lg shadow-2xl"
+        />
+        {/* title and intro */}
+        <div className="px-6">
+          <h1 className="text-3xl font-bold md:text-5xl">
+            Jonathan Brunt: AI Engineer
+          </h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi. Provident cupiditate voluptatem et in. Quaerat fugiat ut
+            assumenda excepturi exercitationem quasi. In deleniti eaque aut
+            repudiandae et a id nisi.
+          </p>
+          {/* link to Projects */}
+          <button className="btn btn-primary">My Projects</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
