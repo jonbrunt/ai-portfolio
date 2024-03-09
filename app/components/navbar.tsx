d632cd0da44602766d79a6c77fc3735e145305e4
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-03-09 20:52:05 -0300

    NAVBAR HANDLE CLICK
    - in components/navbar.tsx:
    - create and export Navbar functional component using React.FC and TS
    - create handleItemClick for menu and conditional rendering
    - use daisyUI `navbar` component for base
    - cusomize navbar with Tailwind utility classes (sticky and top-0)

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -15,3 +15,14 @@ const menuItems: string[] = [
   "YouTube",
   "Contact",
 ];
+
+const Navbar: React.FC<NavbarProps> = ({ setCurrentView }) => {
+  const handleItemClick = (view: string) => {
+    setCurrentView(view);
+    window.scrollTo(0, 0);
+  };
+
+  return <div className="navbar sticky top-0 bg-base-300"></div>;
+};
+
+export default Navbar;
