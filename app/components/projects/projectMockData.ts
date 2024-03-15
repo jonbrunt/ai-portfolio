72b5349a2b0886368b84730390ea9326bd96be1a
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-03-15 09:31:13 -0300

    MOCK DATA FOR COMPONENT
    - in app/components:
    - create `projects` dir
    - in new dir, create projectMockData.ts
    
    - in projectMockData.ts:
    - create a TS interface for the project data object
    - create mock data for 5 projects

---
+++ app/components/projects/projectMockData.ts
@@ -0,0 +1,56 @@
+interface Project {
+  id: number;
+  srcAttr: string;
+  altAttr: string;
+  title: string;
+  description: string;
+  githubBoolean: boolean;
+}
+
+export const projects: Project[] = [
+  {
+    id: 1,
+    srcAttr: "/ai-1.png",
+    altAttr: "Project 1",
+    title: "Project 1",
+    description:
+      "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+    githubBoolean: true,
+  },
+  {
+    id: 2,
+    srcAttr: "/ai-1.png",
+    altAttr: "Project 2",
+    title: "Project 2",
+    description:
+      "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+    githubBoolean: true,
+  },
+  {
+    id: 3,
+    srcAttr: "/ai-1.png",
+    altAttr: "Project 3",
+    title: "Project 3",
+    description:
+      "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+    githubBoolean: true,
+  },
+  {
+    id: 4,
+    srcAttr: "/ai-1.png",
+    altAttr: "Project 4",
+    title: "Project 4",
+    description:
+      "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+    githubBoolean: false,
+  },
+  {
+    id: 5,
+    srcAttr: "/ai-1.png",
+    altAttr: "Project 5",
+    title: "Project 5",
+    description:
+      "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+    githubBoolean: false,
+  },
+];
