0bb8dbaf5ecb53ca6612315568e34964fadb2cc9
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-03-15 10:08:55 -0300

    MAP PROJECT DATA TO RENDER PROJECT CARDS
    - in projects.tsx:
    - map project data and render daisyUI `card`s
    - customize page with Tailwind utility classes
    
    - test in browser

--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -10,6 +10,36 @@ const Projects = () => {
         excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
         id nisi.
       </p>
+      <div className="flex flex-col flex-wrap items-center justify-center gap-4 lg:flex-row">
+        {/* project mapping from projectMockData.ts */}
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="card mb-10 w-96 bg-base-100 shadow-2xl"
+          >
+            <figure>
+              <Image
+                src={project.srcAttr}
+                alt={project.altAttr}
+                width="320"
+                height="320"
+                className="max-w-xs rounded-lg shadow-2xl"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{project.title}</h2>
+              <p>{project.description}</p>
+              <div className="card-actions justify-end">
+                {/* conditional rendering for GitHub button if code is open-source  */}
+                {project.githubBoolean ? (
+                  <button className="btn btn-primary">GitHub</button>
+                ) : null}
+                <button className="btn btn-primary">Open App</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
