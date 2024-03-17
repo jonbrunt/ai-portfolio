4c042f1b2799f2b5e750c6c1899f70a50db5352e
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-03-16 21:05:25 -0300

    BLOG MOCK DATA
    -  in app/components/blog:
    - add blogMockData TS file
    
    - in blogMockData:
    - create a TS interface for the posts
    - populate with mock data for development

---
+++ app/components/blog/blogMockData.ts
@@ -0,0 +1,34 @@
+interface Post {
+  id: number;
+  srcAttr: string;
+  altAttr: string;
+  title: string;
+  description: string;
+}
+
+export const posts: Post[] = [
+  {
+    id: 1,
+    srcAttr: "/blog.png",
+    altAttr: "Post 1",
+    title: "Post 1",
+    description:
+      "Post 1 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+  },
+  {
+    id: 2,
+    srcAttr: "/blog.png",
+    altAttr: "Post 2",
+    title: "Post 2",
+    description:
+      "Post 2 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+  },
+  {
+    id: 3,
+    srcAttr: "/blog.png",
+    altAttr: "Post 3",
+    title: "Post 3",
+    description:
+      "Post 3 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+  },
+];
