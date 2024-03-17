c3991f7b541c45d06da5838a45f93c98afcfbdef
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-03-17 09:47:24 -0300

    ADD MOCK DATA FOR YOUTUBE COMPONENT
    - in app/components:
    - create `youtube` dir
    - add youtubeMockData.ts to the dir
    
    - in youtubeMockData:
    - add a TS interface for the data
    - create unique mock data for three entries

---
+++ app/components/youtube/youtubeMockData.ts
@@ -0,0 +1,34 @@
+export interface Video {
+  id: number;
+  srcAttr: string;
+  altAttr: string;
+  title: string;
+  description: string;
+}
+
+export const videos: Video[] = [
+  {
+    id: 1,
+    srcAttr: "/youtube.png",
+    altAttr: "Video 1",
+    title: "Video 1",
+    description:
+      "Video 1 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+  },
+  {
+    id: 2,
+    srcAttr: "/youtube.png",
+    altAttr: "Video 2",
+    title: "Video 2",
+    description:
+      "Video 2 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+  },
+  {
+    id: 3,
+    srcAttr: "/youtube.png",
+    altAttr: "Video 3",
+    title: "Video 3",
+    description:
+      "Video 3 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+  },
+];
